12d7a0f8ff4c10889f110c08c9fa6f7fcbd7d934
phil <phil@hanbury.(none)>
2009-10-18 17:45:31 +0100

    init

---
+++ html.py
@@ -0,0 +1,39 @@
+
+# HTML library
+# basic level 
+
+def tag(name,x,*argv) :
+	if x is None :
+		return u"<"+name+u"/>"
+	if argv != (None,) : 
+		inside = u''.join(argv)
+	else :
+		inside = u''
+	if isinstance(x,dict) :
+		# we're passing a dictionary of attributes for the tag
+		s = u"<%s " % name 
+		s = s + ' '.join(['%s="%s"'%(k,v) for (k,v) in x.iteritems()])
+		s = s + u">"+inside+u"</"+name+u">"
+		return s
+
+	# or there are no attributes, just inner
+	return u"<"+name+">"+x+inside+"</"+name+">"
+
+
+
+tags = ['html','head','body','script','p','div','table','tr','th','td',
+	'ul','ol','li','dt','dd','h1','h2','h3','h4','h5','h6', 'style']
+
+loc = locals()
+def setit(loc,t) :
+	loc[t] = lambda x=None,*argv : tag(t,x,*argv)
+
+for t in tags :
+	setit(loc,t)
+
+
+# Use like this 
+html(
+	head(),
+	body(),
+)
---
+++ tests.py
@@ -0,0 +1,27 @@
+# Unit tests
+
+import unittest
+from html import html, head, body, p, div, table, th, td, tr, ul, ol, li, dd, dt, h1, h2, h3, h4, h5, h6
+
+class TestHtml(unittest.TestCase) :
+    def test(self) :
+        self.assertEquals(p('hello world'),"<p>hello world</p>")
+        self.assertEquals(div('hello world'),"<div>hello world</div>")
+        self.assertEquals(div({'class':'myclass','id':'myd'},'hello world'),
+                              """<div class="myclass" id="myd">hello world</div>""")
+
+        self.assertEquals(div('a','b'),'<div>ab</div>')
+
+        self.assertEquals(p(),'<p/>')
+
+        print html(
+            head(),
+            body(
+                h2("Header"),
+                p('para1'),
+                p('para2')
+                ))
+
+if __name__ == '__main__' :
+    unittest.main()
+    
