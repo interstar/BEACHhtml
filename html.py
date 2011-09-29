01a25a32b7adbfcd742236ae0ba6db1015b7e528
phil jones <interstar@gmail.com>
2011-09-29 15:38:01 +0100

    added p_ul and p_table

--- html.py
+++ html.py
@@ -20,7 +20,6 @@ def tag(name,x,*argv) :
 	return u"<%s>%s%s</%s>"% (name,x,inside,name)
 
 
-
 tags = ['html','head','body','script','p','div','table','tr','th','td',
 	'ul','ol','li','dt','dd','h1','h2','h3','h4','h5','h6', 'style','b',
 	'pre','hr']
@@ -32,9 +31,39 @@ def setit(loc,t) :
 for t in tags :
 	setit(loc,t)
 
-
 # Use like this 
 html(
 	head(),
 	body(),
 )
+
+def p_ul(x,ys=None) :
+    """Turn a python list into a UL list tag"""
+    if ys : 
+        if isinstance(x,dict) :
+            ys = [li(y) for y in ys]
+            return ul(x,u''.join(ys))
+    return ul(u''.join([li(ox) for ox in x]))
+        
+
+def p_table(x,ys=None) :
+    """Turn a nested python list into a table tag"""
+    tab = x
+    if ys : 
+        if isinstance(x,dict) :
+            tab = ys
+            
+    s = u""
+
+    for row in tab:
+        s += u"""
+<tr>"""+u''.join([td(i) for i in row])+u"""</tr>"""
+
+    if ys :
+        return table(x,s)
+    else :
+        return table(s)             
+
+
+
+
--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 
 import unittest
 from html import html, head, body, p, div, table, th, td, tr, ul, ol, li, dd, dt, h1, h2, h3, h4, h5, h6
+from html import p_ul, p_table
 
 class TestHtml(unittest.TestCase) :
     def test(self) :
@@ -22,6 +23,21 @@ class TestHtml(unittest.TestCase) :
                 p('para2')
                 )),
             """<html><head/><body><h2>Header</h2><p>para1</p><p>para2</p></body></html>""")
+
+    def testList(self) :
+        xs = [1,2,"three"]
+        self.assertEquals(p_ul(xs),"""<ul><li>1</li><li>2</li><li>three</li></ul>""")
+        self.assertEquals(p_ul({"class":"myList"},xs),"""<ul class="myList"><li>1</li><li>2</li><li>three</li></ul>""")
+
+    def testTable(self) :
+        xs = [["oranges","apples","lemons"],[43,65,54]]
+        self.assertEquals(p_table(xs),"""<table>
+<tr><td>oranges</td><td>apples</td><td>lemons</td></tr>
+<tr><td>43</td><td>65</td><td>54</td></tr></table>""")
+        self.assertEquals(p_table({"style":"border-width:3px;"},xs),u"""<table style="border-width:3px;">
+<tr><td>oranges</td><td>apples</td><td>lemons</td></tr>
+<tr><td>43</td><td>65</td><td>54</td></tr></table>""")
+
 
 if __name__ == '__main__' :
     unittest.main()
